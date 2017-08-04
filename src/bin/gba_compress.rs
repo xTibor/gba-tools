736eabf6ba91fe351c3bda889f1cf94da5f8dbcd
Tibor Nagy <xnagytibor@gmail.com>
2017-08-04 15:05:02 +0200

    Add decompression support to gba_compress

--- src/bin/gba_compress.rs
+++ src/bin/gba_compress.rs
@@ -5,22 +5,25 @@ extern crate serde_derive;
 
 use docopt::Docopt;
 use gba_rs::compression::bios::{compress_rle, compress_lz77};
+use gba_rs::compression::bios::{decompress_rle, decompress_lz77};
 use gba_rs::compression::game_specific::wario_land_4::{compress_wl4_rle8, compress_wl4_rle16};
+use gba_rs::compression::game_specific::wario_land_4::{decompress_wl4_rle};
 use gba_rs::utils::streams::{InputStream, OutputStream};
 
 const USAGE: &'static str = "
 Usage:
-    gba_compress --type <type> [--input <input>] [--output <output>]
+    gba_compress --type <type> [--input <input>] [--output <output>] [--decompress]
     gba_compress --help
 
 Options:
-    -i, --input   Input file
-    -o, --output  Place the output into <output>
-    -t, --type    Compression type
-    -h, --help    Display this message
+    -i, --input       Input file
+    -o, --output      Output file
+    -t, --type        Compression type
+    -d, --decompress  Decompression
+    -h, --help        Display this message
 ";
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Copy, Clone)]
 enum CompressionType {
     Lz77,
     Rle,
@@ -33,6 +36,7 @@ struct Args {
     arg_type: CompressionType,
     arg_input: Option<String>,
     arg_output: Option<String>,
+    flag_decompress: bool,
 }
 
 fn main() {
@@ -43,10 +47,21 @@ fn main() {
     let mut input = InputStream::new(args.arg_input).unwrap();
     let mut output = OutputStream::new(args.arg_output).unwrap();
 
-    match args.arg_type {
-        CompressionType::Lz77 => compress_lz77(&mut input, &mut output).unwrap(),
-        CompressionType::Rle => compress_rle(&mut input, &mut output).unwrap(),
-        CompressionType::Wl4Rle8 => compress_wl4_rle8(&mut input, &mut output).unwrap(),
-        CompressionType::Wl4Rle16 => compress_wl4_rle16(&mut input, &mut output).unwrap(),
+    match (args.arg_type, args.flag_decompress) {
+        // Compression
+        (CompressionType::Lz77, false) => compress_lz77(&mut input, &mut output).unwrap(),
+        (CompressionType::Rle, false) => compress_rle(&mut input, &mut output).unwrap(),
+        (CompressionType::Wl4Rle8, false) => compress_wl4_rle8(&mut input, &mut output).unwrap(),
+        (CompressionType::Wl4Rle16, false) => compress_wl4_rle16(&mut input, &mut output).unwrap(),
+        // Decompression
+        (CompressionType::Lz77, true) => decompress_lz77(&mut input, &mut output).unwrap(),
+        (CompressionType::Rle, true) => decompress_rle(&mut input, &mut output).unwrap(),
+        (CompressionType::Wl4Rle8, true) => decompress_wl4_rle(&mut input, &mut output).unwrap(),
+        (CompressionType::Wl4Rle16, true) => decompress_wl4_rle(&mut input, &mut output).unwrap(),
+        // Unsupported
+        _ => {
+            let cmd_str = if args.flag_decompress { "decompression" } else { "compression" };
+            eprintln!("gba_compress: {:?} {} is not supported", args.arg_type, cmd_str);
+        }
     }
 }
