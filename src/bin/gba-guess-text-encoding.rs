4a037250534bdbc2aa0f55badee3734e59c66743
Tibor Nagy <xnagytibor@gmail.com>
2017-08-07 03:59:04 +0200

    Work in progress reimplementation of gba-guess-text-encoding

--- src/bin/gba-guess-text-encoding.rs
+++ src/bin/gba-guess-text-encoding.rs
@@ -1,55 +1,65 @@
-use std::fs::File;
-use std::io::{Cursor, Read, Write, Result, stdout, Error, ErrorKind, Seek, SeekFrom};
-use std::char;
+extern crate docopt;
+extern crate gba_rs;
+#[macro_use]
+extern crate serde_derive;
 
-// A tool to guess a game's character encoding
-// Expects a text string used in the game, outputs the possible encoding tables.
-// Very work in progress.
+use docopt::Docopt;
+use gba_rs::utils::format::format_offset;
+use gba_rs::utils::streams::{InputStream, OutputStream};
+use std::io::{Read, Write};
 
-// TODO: Cleanup
-// TODO: Explain how it works (relative letter distances)
-// TODO: Check the following formats: u8, u16_le, u16_be, u16_hi_byte, u16_lo_byte
-// TODO: Command-line interface
-// TODO: Export the possible encodings in a standard format
-// TODO: Handle capital letters
-// TODO: Group same encodings
-// TODO: Write tests
+const USAGE: &'static str = "
+Usage:
+    gba-guess-text-encoding --string <string> [--input <input>] [--output <output>] [--hex]
+    gba-guess-text-encoding --help
 
-fn find_text_encoding() -> Result<()> {
-    let mut rom_data: Vec<u8> = Vec::new();
-    let mut rom_file = File::open("rom/wario_land_4.gba")?;
-    rom_file.read_to_end(&mut rom_data)?;
+Options:
+    -s, --string <string>  String to search for
+    -i, --input <input>    Input file
+    -o, --output <output>  Output file
+    -H, --hex              Use hexadecimal offsets
+    -h, --help             Display this message
+";
 
-    let mut rom_diff: Vec<u8> = Vec::new();
-    rom_diff.push(0);
-    for i in 1..rom_data.len() {
-        rom_diff.push(rom_data[i].wrapping_sub(rom_data[i - 1]));
-    }
-
-    let searchstring = "ieroglyphs";
-    let searchstring_bytes: Vec<u8> = searchstring.as_bytes().to_vec();
-    let mut searchstring_diff: Vec<u8> = Vec::new();
-    for i in 1..searchstring_bytes.len() {
-        searchstring_diff.push(searchstring_bytes[i].wrapping_sub(searchstring_bytes[i - 1]));
-    }
-
-    for (offset, w) in rom_diff.windows(searchstring_diff.len()).enumerate() {
-        if *w == *&searchstring_diff[..] {
-            println!("Found at offset 0x{:08X}", offset);
-            println!("Possible text encoding:");
-            for char_index in 0..26 {
-                let c = char::from_u32(0x61 + char_index).unwrap();
-                let b = rom_data[offset-1].wrapping_sub(searchstring_bytes[0].wrapping_sub(0x61)).wrapping_add(char_index as u8);
-
-                println!("\"{}\" => 0x{:02X}", c, b);
-            }
-            println!();
-        }
-    }
-
-    Ok(())
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_string: String,
+    flag_input: Option<String>,
+    flag_output: Option<String>,
+    flag_hex: bool,
 }
 
 fn main() {
-    find_text_encoding().unwrap();
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    if args.flag_string.len() < 2 {
+        eprintln!("gba-guess-text-encoding: `--string` must be at least 2 characters long");
+        return
+    }
+
+    let mut input = InputStream::new(args.flag_input).unwrap();
+    let mut output = OutputStream::new(args.flag_output).unwrap();
+
+    let mut input_data = Vec::new();
+    input.read_to_end(&mut input_data).unwrap();
+
+    let needle_diffs = args.flag_string
+        .as_bytes()
+        .windows(2)
+        .map(|window| window[1] as isize - window[0] as isize)
+        .collect::<Vec<isize>>();
+
+    let haystack_diffs = input_data
+        .windows(2)
+        .map(|window| window[1] as isize - window[0] as isize)
+        .collect::<Vec<isize>>();
+
+    for (offset, window) in haystack_diffs.windows(needle_diffs.len()).enumerate() {
+        if *window == needle_diffs[..] {
+            let offset_str = format_offset(offset, args.flag_hex);
+            writeln!(output, "{}", offset_str).unwrap();
+        }
+    }
 }
